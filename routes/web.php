c33bd9009eeb118b9bb660788a35ab5aba8e95bf
VeronicaBeMay-218 <veronica.19070007@itsmotul.edu.mx>
2023-03-18 23:40:24 -0600

    array db

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserCrudController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +20,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/saludo', function () {
+    return 'Hello World';
+});
+
+Route::get('/user', [UserController::class, 'index']);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+  //diferentes metodos
+
+    Route::resource('/users', UserCrudController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
